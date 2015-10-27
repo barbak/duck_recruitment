1d445729baa20afbfe11613b74ec4ac801d07516
paul guichon <paul.guichon@iedparis8.net>
2015-10-27 16:25:03 +0100

    filtre par 510

--- duck_recruitment/management/commands/titulaire.py
+++ duck_recruitment/management/commands/titulaire.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 # from __future__ import unicode_literals
-from django_apogee.models import InsAdmEtp, Individu, Adresse, AnneeUni, ConfAnneeUni, EtpGererCge, InsAdmEtpInitial, \
-    Etape
 import csv
-from duck_recruitment.models import Ec, EtapeVet, Titulaire
-import json
-__author__ = 'paul'
-j= """[["BRIDOU", "Morgiane", "", "morgiane.bridou@iedparis8.net"], ["Arnaud", "Plagnol", "", "arnaud.plagnol@iedparis8.net"], ["Lionel", "Dagot", null, "lionel.dagot@iedparis8.net"], ["AMORIM", "Marilia", "DUFOUR", "mamorim@univ-paris8.fr"], ["Antoine", "Corinne", "", "corinne.antoine@iedparis8.net"], ["Baque", "Dominique", "", "dominique.baque@iedparis8.net"], ["Meunier", "Jean-Marc", "", "jean-marc.meunier@iedparis8.net"], ["Bardin", "Brigitte", "", "brigitte.bardin@univ-tlse2.fr"], ["Behra", "Timoth\\u00e9e", "", "timotheebehra.etu@gmail.com"], ["Bernard", "Gilles", "", "gilles.bernard@iedparis8.net"], ["COLIN", "Lucette", "", "lucettecolin@noos.fr"], ["CONTY", "LAurence", "Conty", "laurence.conty@gmail.com"], ["Chicharro", "Gladys", "", "gladyschicharro@hotmail.com"], ["DEBORDE", "Anne-Sophie", "", "anne-spohie.deborde@iedparis8.net"], ["DEMARCHI", "SAMUEL", "", "samuel.demarchi@univ-paris8.fr"], ["DUPUCH", "LAURENCE", "", "laurence.dupuch02@univ-paris8.fr"], ["DURIEZ", "NATHALIE", "TIQUET", "nathalie.duriez-tiquet@univ-paris8.fr"], ["DURIEZ", "NATHALIE", "TIQUET", "nathalie.duriez@iedparis8.net"], ["Dominique", "Baqu\\u00e9", null, "dombaque@orange.fr"], ["ELOY", "Florence", "", "florence_eloy@yahoo.fr"], ["Eriksen", "Anna", "Terzian", "anna.terzian@univ-paris8.fr"], ["Francoise", "Morange-Majoux", "Majoux", "francoise.morange-majoux@iedparis8.net"], ["GUENIF", "NACIRA", "SOUILAMAS", "nacira.guenif@univ-paris8.fr"], ["GUERAUD", "SABINE", "", "sabine.gueraud@univ-paris8.fr"], ["HENNION", "PATRICIA", "JACQUET", "patricia.hennion-jacquet@iedparis8.net"], ["HESS", "Remi", "", "remihess@noos.fr"], ["HOUILLON", "Jean-Charles", "", "jean-charles.houillon@iedparis8.net"], ["Habib", "Marianne", "", "marianne.habib@univ-paris8.fr"], ["Jean", "Laingui", null, "jean.laingui@iedparis8.net"], ["Juhan", "Michel", "", "michel.juhan@univ-paris8.fr"], ["LEFEVRE", "CAROLE", "", "carole.lefevre@iedparis8.net"], ["Laurent", "Jean-Paul", "", "jean-paul.laurent@iedparis8.net"], ["Lesourd", "Francis", "", "francis.lesourd@iedparis8.net"], ["Lujan", "Cristianh", "", "christian.lujan@iedparis8.net"], ["MARIAGE", "Jean-Jacques", "", "jjmariage@free.fr"], ["MASSE", "LAURENCE", "", "laurence.masse@iedparis8.net"], ["Gautier", "Agn\\u00e8s", "Gautier-Audebert", "agnes.gautier.audebert@neuf.fr"], ["MOREAU", "Didier", "", "didier.moreauparis8@gmail.com"], ["Marquez-D.", "Eduardo", "", "eduardo.marquez@iedparis8.net"], ["Mehat", "Jean", "", "jmehat@gmail.com"], ["Monica", "Macedo", "", "monica.macedo@iedparis8.net"], ["Morisse", "Martine", "", "martine.morisse@iedparis8.net"], ["Morisse", "Martine", "", "martine.morisse@univ-paris8.fr"], ["NIVARD", "Jacqueline", "BIGOURDAN", "jacqueline.nivard@iedparis8.net"], ["PAPPA", "ANNA", "", "ap@ai.univ-paris8.fr"], ["Perrotin", "Marie-Liesse", "", "marie-liesse.perrotin@iedparis8.net"], ["STILGENBAUER", "Jean-Louis", "", "jean-louis.stilgenbauer@iedparis8.net"], ["Villemonteix", "Thomas", "", "t.villemonteix@gmail.com"], ["Vittaut", "Jean-No\\u00ebl", "", "jean-noel.vittaut@univ-paris8.fr"], ["Wertz", "Harald", "", "hw@ai.univ-paris8.fr"], ["YOUEGO", "CHRISTINE", "", "moyoue@yahoo.fr"], ["berthier", "patrick", "", "patrick.berthier@yahoo.fr"], ["chaumet", "pierre-olivier", "chaumet", "pochaumet@hotmail.fr"], ["danjaume", "g\\u00e9raldine", "racchini", "gracchini@free.fr"], ["feat", "jym", "", "jym.feat@iedparis8.net"], ["galand", "charles", "", "charles.galand@hotmail.fr"], ["leclere", "maryvonne", "", "maryvonne.leclere@iedparis8.net"], ["marion", "catherine", "Lissajoux", "catherine.marion@iedparis8.net"], ["molinie", "magali", "", "magali.molinie@iedparis8.net"], ["mounard", "michel", "", "michelmounard@hotmail.com"], ["raphaele", "miljkovitch", "", "raphaele.miljkovitch@iedparis8.net"], ["silke", "schauder", "", "silke.schauder@iedparis8.net"], ["vaillant", "Laurence", "", "laurence.vaillant02@univ-paris8.fr"]]"""
 
+from django_apogee.models import InsAdmEtp, Individu
 from django.core.management.base import BaseCommand
 from optparse import make_option
-from django.db import connections
 import requests
 import simpleldap
+
+
+ANNEE = 2015
+
 class Command(BaseCommand):
 
-
+    url = 'http://moodle.iedparis8.net/webservice/rest/server.php'
+    token = '91361080b67ff676c3d7789e9e967ca2'
     option_list = BaseCommand.option_list + (
         make_option('--annee',
                     action='store',
@@ -25,90 +24,149 @@ class Command(BaseCommand):
                     help='annee de remontee'),
     )
     'npeteinatos;not cached;nikos;peitenatos;nikos.peteinatos@iedparis8.net;cas;14511794;fr;L1NPSY;1'
-    def handle(self, *args, **options):
-        url = 'http://moodle.iedparis8.net/webservice/rest/server.php'
-        wsfunction = 'core_user_create_users'
-        param = {
-            'wstoken': '91361080b67ff676c3d7789e9e967ca2',
-            'wsfunction': wsfunction,
-            'moodlewsrestformat': 'json',
-            'users[0][username]': 'npeteinatos',
-            'users[0][password]': 'not cached',
-            'users[0][firstname]': 'nikos',
-            'users[0][lastname]': 'peitenatos',
-            'users[0][email]': 'nikos.peteinatos@iedparis8.net',
-            'users[0][auth]': 'cas',
-            'users[0][idnumber]': '14511794',
-            'users[0][lang]': 'fr',
-            # '': '',
 
+    def get_ldap_student(self, cod_etp):
+        filtre = '(&(uid=*)(mail=*etud*)(up8Diplome={}))'.format(cod_etp)
+        attr = [
+            'sn',
+            'givenName',
+            'supannEtuId',
+            'uid'
+        ]
+        search = {'base_dn': 'dc=univ-paris8,dc=fr', 'list': 'sn,givenName,supannEtuId'}
+        conn = simpleldap.Connection('ldap.etud.univ-paris8.fr',
+                                     dn='cn=admin,dc=univ-paris8,dc=fr',
+                                     search_defaults=search,
+                                     password='p8SARiH3')
+        results = conn.search(filtre,
+                              # attrs=attr
+                              )
+        results = {x['supannEtuId'][0]: x['uid'][0] for x in results}
+        return results
+
+    def get_all_user(self, cod_etp):
+        resultats = []
+        ldap_user = self.get_ldap_student(cod_etp)
+        codes_etu = ldap_user.keys()
+        for individu in Individu.objects.using('oracle').filter(etapes__cod_etp=cod_etp, etapes__cod_anu=ANNEE,
+                                                                etapes__eta_iae='E', etapes__tem_iae_prm='O'):
+            if str(individu.cod_etu) in codes_etu:
+                cod_etu = str(individu.cod_etu)
+                user = {
+                    'username': ldap_user[cod_etu],
+                    'firstname': individu.lib_pr1_ind,
+                    'lastname': individu.lib_nom_pat_ind,
+                    'cod_etu': cod_etu
+                }
+                resultats.append(user)
+        return resultats
+    def get_all_user_csv(self, users, cod_etp):
+        result = [[user['username'], 'not cached', user['firstname'],
+                  user['lastname'], '{}@foad.iedparis8.net'.format(user['cod_etu']),
+                   'cas', user['cod_etu'], 'fr', cod_etp, 1] for user in users]
+        return result
+
+    def create_user(self, user):
+        param = {
+            'wstoken': self.token,
+            'wsfunction': 'core_user_create_users',
+            'moodlewsrestformat': 'json',
+            'users[0][username]': user['username'],
+            'users[0][password]': 'not cached',
+            'users[0][firstname]': user['firstname'],
+            'users[0][lastname]': user['lastname'],
+            'users[0][email]': '{}@foad.iedparis8.net'.format(user['cod_etu']),
+            'users[0][auth]': 'cas',
+            'users[0][idnumber]': user['cod_etu'],
+            'users[0][lang]': 'fr',
         }
-        p = {
-            'wstoken': '91361080b67ff676c3d7789e9e967ca2',
+        result = requests.post(self.url, data=param).json()
+        return result
+
+    def create_users(self, users):
+        param = {
+            'wstoken': self.token,
+            'wsfunction': 'core_user_create_users',
+            'moodlewsrestformat': 'json'
+        }
+        for i, user in enumerate(users[:10]):
+            param['users[{}][username]'.format(i)] = user['username']
+            param['users[{}][password]'.format(i)] = 'not cached'
+            param['users[{}][firstname]'.format(i)] = user['firstname']
+            param['users[{}][lastname]'.format(i)] = user['lastname']
+            param['users[{}][email]'.format(i)] = '{}@foad.iedparis8.net'.format(user['cod_etu'])
+            param['users[{}][auth]'.format(i)] = 'cas'
+            param['users[{}][idnumber]'.format(i)] = user['cod_etu']
+            param['users[{}][lang]'.format(i)] = 'fr'
+        result = requests.post(self.url, data=param).text
+        return result
+
+    def get_user(self, user):
+        param = {
+            'wstoken': self.token,
             'wsfunction': 'core_user_get_users_by_field',
             'moodlewsrestformat': 'json',
             'field': 'username',
-            'values[0]': 'npeteinatos'
+            'values[0]': user['username']
         }
-        result = requests.post(url, data=p).json()[0]
+        result = requests.post(self.url, data=param).json()[0]
+        return result
 
-        param['wsfunction'] = 'core_user_update_users'
-        print result['id']
-        param['users[0][id]'] = result['id']
-        param.pop('users[0][password]')
-
-        print param
-        result = requests.post(url, data=param).json()
-
-        print result
+    def update_user(self, user):
         param = {
-                'wstoken': '91361080b67ff676c3d7789e9e967ca2',
+            'wstoken': self.token,
+            'wsfunction': 'core_user_update_users',
+            'moodlewsrestformat': 'json',
+            'users[0][id]': user['id'],
+            'users[0][username]': user['username'],
+            'users[0][firstname]': user['firstname'],
+            'users[0][lastname]': user['lastname'],
+            'users[0][email]': '{}@foad.iedparis8.net'.format(user['cod_etu']),
+            'users[0][auth]': 'cas',
+            'users[0][idnumber]': user['cod_etu'],
+            'users[0][lang]': 'fr',
+        }
+        result = requests.post(self.url, data=param).json()
+        return result
+
+    def add_cohorts(self, user, cod_etp):
+        param = {
+                'wstoken': self.token,
                 'wsfunction': 'core_cohort_add_cohort_members',
                 'moodlewsrestformat': 'json',
                 'members[0][cohorttype][type]': 'idnumber',
-                'members[0][cohorttype][value]':  'L3NEDU',
+                'members[0][cohorttype][value]':  cod_etp,
                 'members[0][usertype][type]': 'username',
-                'members[0][usertype][value]': 'npeteinatos',
+                'members[0][usertype][value]': user['username'],
             }
-        result = requests.post(url, data=param).json()
-        print result
+        result = requests.post(self.url, data=param).json()
+        return result
 
-        param = {
-                'wstoken': '91361080b67ff676c3d7789e9e967ca2',
-                'wsfunction': 'core_cohort_add_cohort_members',
-                'moodlewsrestformat': 'json',
-                'members[0][cohorttype][type]': 'idnumber',
-                'members[0][cohorttype][value]':  'L3NEDU',
-                'members[0][usertype][type]': 'username',
-                'members[0][usertype][value]': 'npeteinatos',
-            }
-        result = requests.post(url, data=param).json()
-        print result
-        # if False:
-        #     filtre = '(&(uid=*)(mail=*etud*)(up8Diplome=L3NEDU))'
-        #     attr = [
-        #         'sn',
-        #         'givenName',
-        #         'supannEtuId',
-        #         'uid'
-        #     ]
-        #     search = {'base_dn': 'dc=univ-paris8,dc=fr', 'list': 'sn,givenName,supannEtuId'}
-        #     conn = simpleldap.Connection('ldap.etud.univ-paris8.fr',
-        #                                  dn='cn=admin,dc=univ-paris8,dc=fr',
-        #                                  search_defaults=search,
-        #                                  password='p8SARiH3')
-        #     results = conn.search(filtre,
-        #                           # attrs=attr
-        #                           )
-        #     # print len(results)
-        #     r = {}
-        #     with open('eggs.csv', 'wb') as csvfile:
-        #         spamwriter = csv.writer(csvfile, delimiter=' ',
-        #                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        #
-        #         for x in results:
-        #             spamwriter.writerow([x['supannEtuId'][0], x['uid'][0]])
-        # else:
+    def remontee_user(self, user, cod_etp):
+        print self.create_user(user)
+        r = self.get_user(user)
+        user['id'] = r['id']
+        print self.update_user(user)
+        # print self.add_cohorts(user, cod_etp)
+
+    def create_csv(self, users, cod_etp):
+        with open('/vagrant/cohorts_{}.csv'.format(cod_etp), 'wb') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=';',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            champs = ['username', 'password', 'firstname', 'lastname',
+                      'email', 'auth', 'idnumber', 'lang', 'cohort1', 'type1']
+            spamwriter.writerow(champs)
+            for row in users:
+                spamwriter.writerow(row)
+
+    def handle(self, *args, **options):
+        cod_etp = 'M1NEFI'
+        users = self.get_all_user(cod_etp)
+        # self.create_users(users)
+        self.create_csv(self.get_all_user_csv(users, cod_etp), cod_etp)
+
+
+
         #     result = {}
         #     with open('eggs.csv', 'rb') as csvfile:
         #         spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
@@ -129,3 +187,4 @@ class Command(BaseCommand):
         #         spamwriter.writerow(champs)
         #         for row in r:
         #             spamwriter.writerow(row)
+
