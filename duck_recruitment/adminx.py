5c8b01ee161067a98b9075a18f1bc1c685d0751e
paul guichon <paul.guichon@iedparis8.net>
2016-02-24 11:05:33 +0100

    mise à jour des fonctionnalité de impression etrange et dom-tom

--- duck_recruitment/adminx.py
+++ duck_recruitment/adminx.py
@@ -38,59 +38,59 @@ class TypeEcsAdmin(object):
     style = 'table'
     extra = 1
 
-class HeureForfaitAdmin(object):
-    model = HeureForfait
-    style = 'table'
-    extra = 1
-
-    @filter_hook
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        # If it uses an intermediary model that isn't auto created, don't show
-        # a field in admin.
-        if isinstance(db_field, models.ManyToManyField) and not db_field.rel.through._meta.auto_created:
-            return None
-        attrs = self.get_field_attrs(db_field, **kwargs)
-        if db_field.name == 'type_ec':
-            kwargs['queryset'] = self.model_instance.typeec_set.all()
-        return db_field.formfield(**dict(attrs, **kwargs))
-
-class EtapeVetAdmin(object):
-    fields = ('cod_etp',)
-    readonly_fields = ('cod_etp',)
-    inlines = [TypeEcsAdmin, HeureForfaitAdmin]
-
-    def queryset(self):
-        queryset = super(EtapeVetAdmin, self).queryset()
-        return queryset.filter(cod_vrs_vet__in=[510,520])
-
-    # def
-
-class EtapeFilter(RelatedFieldListFilter):
-    def choices(self):
-        self.lookup_choices = [(x.pk, x.cod_etp.lib_etp) for x
-                               in EtapeVet.objects.filter(cod_vrs_vet__in=[510, 520]).order_by('cod_etp__lib_etp')]
-        return super(EtapeFilter, self).choices()
-
-class EcAdmin(object):
-    list_filter = [('etape', EtapeFilter)]
-    fields = ('type',)
-
-    @filter_hook
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        # If it uses an intermediary model that isn't auto created, don't show
-        # a field in admin.
-        if isinstance(db_field, models.ManyToManyField) and not db_field.rel.through._meta.auto_created:
-            return None
-        attrs = self.get_field_attrs(db_field, **kwargs)
-        if db_field.name == 'type':
-            kwargs['queryset'] = self.org_obj.etape.filter(cod_vrs_vet__in=[510,520]).first().typeec_set.all()
-                # kwargs['queryset'] = TypeEc.objects.filter(etape__pk=pk)
-                # print kwargs['queryset']
-        return db_field.formfield(**dict(attrs, **kwargs))
-
-    def queryset(self):
-        queryset = super(EcAdmin, self).queryset()
-        return queryset.filter(etape__cod_vrs_vet__in=[510, 520])
-
-xadmin.site.register(EtapeVet, EtapeVetAdmin)
-xadmin.site.register(Ec, EcAdmin)
+# class HeureForfaitAdmin(object):
+#     model = HeureForfait
+#     style = 'table'
+#     extra = 1
+#
+#     @filter_hook
+#     def formfield_for_dbfield(self, db_field, **kwargs):
+#         # If it uses an intermediary model that isn't auto created, don't show
+#         # a field in admin.
+#         if isinstance(db_field, models.ManyToManyField) and not db_field.rel.through._meta.auto_created:
+#             return None
+#         attrs = self.get_field_attrs(db_field, **kwargs)
+#         if db_field.name == 'type_ec':
+#             kwargs['queryset'] = self.model_instance.typeec_set.all()
+#         return db_field.formfield(**dict(attrs, **kwargs))
+#
+# class EtapeVetAdmin(object):
+#     fields = ('cod_etp',)
+#     readonly_fields = ('cod_etp',)
+#     inlines = [TypeEcsAdmin, HeureForfaitAdmin]
+#
+#     def queryset(self):
+#         queryset = super(EtapeVetAdmin, self).queryset()
+#         return queryset.filter(cod_vrs_vet__in=[510,520])
+#
+#     # def
+#
+# class EtapeFilter(RelatedFieldListFilter):
+#     def choices(self):
+#         self.lookup_choices = [(x.pk, x.cod_etp.lib_etp) for x
+#                                in EtapeVet.objects.filter(cod_vrs_vet__in=[510, 520]).order_by('cod_etp__lib_etp')]
+#         return super(EtapeFilter, self).choices()
+#
+# class EcAdmin(object):
+#     list_filter = [('etape', EtapeFilter)]
+#     fields = ('type',)
+#
+#     @filter_hook
+#     def formfield_for_dbfield(self, db_field, **kwargs):
+#         # If it uses an intermediary model that isn't auto created, don't show
+#         # a field in admin.
+#         if isinstance(db_field, models.ManyToManyField) and not db_field.rel.through._meta.auto_created:
+#             return None
+#         attrs = self.get_field_attrs(db_field, **kwargs)
+#         if db_field.name == 'type':
+#             kwargs['queryset'] = self.org_obj.etape.filter(cod_vrs_vet__in=[510,520]).first().typeec_set.all()
+#                 # kwargs['queryset'] = TypeEc.objects.filter(etape__pk=pk)
+#                 # print kwargs['queryset']
+#         return db_field.formfield(**dict(attrs, **kwargs))
+#
+#     def queryset(self):
+#         queryset = super(EcAdmin, self).queryset()
+#         return queryset.filter(etape__cod_vrs_vet__in=[510, 520])
+#
+# xadmin.site.register(EtapeVet, EtapeVetAdmin)
+# xadmin.site.register(Ec, EcAdmin)
